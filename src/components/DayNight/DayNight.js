eb6fead82be0a50abdc033ee8c1b0d21459208e8
krill <lpmaze@gmail.com>
2019-12-05 16:51:00 -0500

    adjusted night colors

--- src/components/DayNight/DayNight.js
+++ src/components/DayNight/DayNight.js
@@ -1,18 +1,17 @@
 import React, { useContext } from 'react'
 import './DayNight.css'
 import { ThemeContext } from '../../contexts/ThemeContext'
-import { NavContext } from '../../contexts/NavContext'
 
 const DayNight = () => {
   const { dayNight, toggleDayNight } = useContext(ThemeContext)
-  const {expanded} = useContext(NavContext)
 
   const handleDayNightClick = () => {
     toggleDayNight()
   }
 
   return (
-    <div className={`DayNight ${expanded?'DayNight-slide':''}`} onClick={handleDayNightClick}>
+    <div className="DayNight" onClick={handleDayNightClick}>
+      <div className={`DayNight ${dayNight ? '' : 'DayNight-night'}`}></div>
       <div
         className={`DayNight-moon ${dayNight ? '' : 'DayNight-moon-active'}`}
       ></div>
